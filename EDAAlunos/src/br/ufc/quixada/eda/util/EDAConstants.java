e8aee0c9e14d944966c8a8d000f4e3e85f035ac1
yaagocruuz <yaagocruuz@users.noreply.github.com>
2017-07-10 16:13:01 -0300

    Update EDAConstants.java

--- EDAAlunos/src/br/ufc/quixada/eda/util/EDAConstants.java
+++ EDAAlunos/src/br/ufc/quixada/eda/util/EDAConstants.java
@@ -1,5 +1,5 @@
 package br.ufc.quixada.eda.util;
 
 public interface EDAConstants {
-	String caminhoPasta = System.getProperty("user.dir") + "/Instancias/instancesALM/";
+	String caminhoPasta = System.getProperty("user.dir") + "/Instancias/instanciasArvores/";
 }
